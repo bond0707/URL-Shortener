b97731b61950851ccc88819abfd6305f79bb3022
bond0707 <kirtanbuch007@gmail.com>
2024-09-22 02:26:00 +0530

    Initial Commit

---
+++ URLShortener.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+class URLShortener():
+    url_dict = {}
+    charset = list(string.ascii_letters + string.digits)
+
+    def __init__(self):
+        pass
+        
+    def make_key(self):
+        return "".join(random.choices(URLShortener.charset, k=6))
+
+    def shorten_url(self, url: str):
+        key = self.make_key()
+        while key in URLShortener.url_dict.keys():
+            key = self.make_key()
+
+        URLShortener.url_dict[key] = url
+        return key
+
+    def give_url_from_key(self, key: str):
+        try:
+            return URLShortener.url_dict[key]
+        except KeyError:
+            return "Your link has expired."
+
+
+if __name__ == "__main__":
+    obj = URLShortener()
+    print(obj.shorten_url("Hi"))
+    print(obj.give_url_from_key(obj.shorten_url("Hi")))
---
+++ WebUI.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from urllib.parse import urlparse
+from URLShortener import URLShortener
+
+# python -m streamlit run WebUI.py --theme.base=dark
+
+def is_url(url):
+  try:
+    result = urlparse(url)
+    return all([result.scheme, result.netloc])
+  except ValueError:
+    return False
+
+if __name__ == "__main__":
+    obj = URLShortener()
+
+    query_params = st.query_params
+    if "key" in query_params.keys():
+        url = obj.give_url_from_key(query_params["key"])
+        st.link_button(
+            label="Click here if you are not being redirected automatically.",
+            url=url,
+            use_container_width=True
+        )
+    else:
+        st.title("URL Shortener")
+        url = st.text_input("Enter your URL")
+        submit_btn = st.button("Submit", use_container_width=True)
+
+        if submit_btn:
+            if not is_url(url):
+                st.error("Please enter a valid URL!")
+            else:
+                st.write("Shortened URL")
+                st.success("http://localhost:8501/?key=" + obj.shorten_url(url))
